14b8774680ed1330e28544b2d5c77a9a0517ac0a
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2025-11-19 16:56:10 +0800

    Add new hospital and invoice routes
    Introduced GET route for hospital invoices with parameters and DELETE route for hospital deletion. Updated invoice index route to accept processing_days as a parameter.

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,12 @@ Route::get('/invoice_page', [InvoiceController::class, 'invoicePage']);
 Route::prefix("hospitals")->group(function () {
     Route::get("/", [HospitalController::class, "index"]);
     Route::post("/create", [HospitalController::class, "store"]);
+    Route::get("/invoices/{hospital_id}/{processing_days}/{invoices_count}", [HospitalController::class, "show"]);
     Route::put("/edit/{id}", [HospitalController::class, "update"]);
+    Route::delete("/delete/{id}", [HospitalController::class, "destroy"]);
 });
 
 Route::prefix("invoices")->group(function () {
-    Route::get("/", [InvoiceController::class, "index"]);
+    Route::get("/{processing_days}", [InvoiceController::class, "index"]);
 });
 
