bbe8b1b20ad8c01b08dc5dffc498ccb8e38ef69b
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2025-11-19 16:52:40 +0800

    Add soft delete for deleting data

--- database/migrations/2025_11_13_000003_create_hospitals_table.php
+++ database/migrations/2025_11_13_000003_create_hospitals_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('hospital_name');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
