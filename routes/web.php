aa03287bbb6e17ad5f8c9509aac47c5527018ca4
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2025-11-20 17:01:40 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,12 @@ Route::get('/invoice_page', [InvoiceController::class, 'invoicePage']);
 Route::prefix("hospitals")->group(function () {
     Route::get("/", [HospitalController::class, "index"]);
     Route::post("/create", [HospitalController::class, "store"]);
-    Route::get("/invoices/{hospital_id}/{processing_days}/{invoices_count}", [HospitalController::class, "show"]);
     Route::put("/edit/{id}", [HospitalController::class, "update"]);
     Route::delete("/delete/{id}", [HospitalController::class, "destroy"]);
-});
-
+    Route::get("/invoices/{hospital_id}/{processing_days}/{invoices_count}", [HospitalController::class, "show"]);
+    Route::get("/{hospital_id}/invoices/create", [HospitalController::class, "createInvoice"]);
+    Route::post("/invoices/store", [HospitalController::class, "storeInvoice"]);
+}); 
 Route::prefix("invoices")->group(function () {
     Route::get("/{processing_days}", [InvoiceController::class, "index"]);
 });
