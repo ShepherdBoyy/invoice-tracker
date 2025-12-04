ab448e17651e9721981192644f41396eabda6d5b
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2025-12-04 17:01:20 +0800

    Add TemplateExport class for Excel exports
    Introduces TemplateExport implementing FromArray, WithHeadings, and ShouldAutoSize for exporting invoice templates with predefined headings and empty data array.

---
+++ app/Exports/TemplateExport.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Invoice;
+use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class TemplateExport implements FromArray, WithHeadings, ShouldAutoSize
+{
+    public function headings(): array
+    {
+        return [
+            "Customer No.",
+            "Customer Name",
+            "Invoice No.",
+            "Document Date",
+            "Due Date",
+            "Days Overdue",
+            "Amount"
+        ];
+    }
+
+    public function array(): array
+    {
+        return [];
+    }
+}
