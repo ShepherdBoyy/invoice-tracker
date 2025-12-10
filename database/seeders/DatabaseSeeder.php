fe405f1deb6a894ae038da358127083ff6fabaa2
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2025-12-10 16:53:56 +0800

    Update DatabaseSeeder.php
    Seed the database with only 5 users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Area;
 use App\Models\Hospital;
 use App\Models\Invoice;
 use App\Models\InvoiceHistory;
@@ -14,22 +15,35 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->count(5)->create();
+        $areas = Area::factory()->count(10)->create();
 
-        User::firstOrCreate(
-            ['username' => 'admin'],
-            [
-                'name' => 'Administrator',
-                'role' => 'purchasing',
-                'password' => Hash::make("password"),
-                "visible_password" => Crypt::encryptString("password"),
-            ]
-        );
+        $roles = [
+            ['username' => 'admin', 'name' => 'Admin User', 'role' => 'admin'],
+            ['username' => 'purchasing', 'name' => 'Purchasing User', 'role' => 'purchasing'],
+            ['username' => 'collector', 'name' => 'Collector User', 'role' => 'collector'],
+            ['username' => 'agent', 'name' => 'Agent User', 'role' => 'agent'],
+            ['username' => 'accounting', 'name' => 'Accounting User', 'role' => 'accounting'],
+        ];
+
+        foreach ($roles as $userData) {
+            User::firstOrCreate(
+                ['username' => $userData['username']],
+                [
+                    'name' => $userData['name'],
+                    'role' => $userData['role'],
+                    'password' => Hash::make('password'),
+                    'area_id' => $areas->random()->id,
+                    'visible_password' => Crypt::encryptString('password'),
+                ]
+            );
+        }
 
         Hospital::factory()
             ->count(50)
             ->has(Invoice::factory()->count(15))
-            ->create();
+            ->create([
+                'area_id' => $areas->random()->id,
+            ]);
 
         $users = User::pluck('id')->toArray();
 
