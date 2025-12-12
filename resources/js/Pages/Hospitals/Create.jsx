51805294b64c6747d677f7cd5da513a6e31cc9f8
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2025-12-12 14:17:30 +0800

    Add area selection to hospital create and edit forms
    Introduces an area dropdown to both the Create and Edit hospital modals, allowing users to select an area when creating or editing a hospital. Error handling for area selection is also included, and UI adjustments were made for consistency and improved user experience.

--- resources/js/Pages/Hospitals/Create.jsx
+++ resources/js/Pages/Hospitals/Create.jsx
@@ -5,11 +5,11 @@ export default function Create({
     setOpenCreateModal,
     setShowToast,
     setSuccessMessage,
+    areas,
 }) {
     const [error, setError] = useState("");
     const [hospitalName, setHospitalName] = useState("");
 
-    console.log(error)
 
     return (
         <dialog open className="modal">
@@ -32,7 +32,10 @@ export default function Create({
                 >
                     <div className="flex flex-col gap-1 mt-8">
                         <div className="flex justify-between">
-                            <label htmlFor="hospital_number" className="text-sm">
+                            <label
+                                htmlFor="hospital_number"
+                                className="text-sm"
+                            >
                                 Hospital No.
                             </label>
                             {error.hospital_number && (
@@ -70,7 +73,43 @@ export default function Create({
                         />
                     </div>
 
-                    <div className="flex justify-end mt-6">
+                    <div className="flex flex-col gap-2 mt-8">
+                        <div className="flex justify-between">
+                            <label htmlFor="area_id" className="text-sm">
+                                Area:
+                            </label>
+                            {error.area_id && (
+                                <span className="text-red-500 text-sm">
+                                    {error.area_id}
+                                </span>
+                            )}
+                        </div>
+                        <select
+                            defaultValue=""
+                            className="select w-full"
+                            name="area_id"
+                            id="area_id"
+                        >
+                            <option value="" disabled>
+                                Select
+                            </option>
+                            {areas.map((area) => (
+                                <option key={area.id} value={area.id}>
+                                    {area.area_name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <div className="flex justify-end mt-6 gap-2">
+                        <button
+                            className="btn btn-outline rounded-xl"
+                            onClick={() => {
+                                setOpenCreateModal(false);
+                            }}
+                        >
+                            Cancel
+                        </button>
                         <button
                             type="submit"
                             className="btn bg-gray-800 text-white rounded-xl "
--- resources/js/Pages/Hospitals/Edit.jsx
+++ resources/js/Pages/Hospitals/Edit.jsx
@@ -1,9 +1,17 @@
 import { Form } from "@inertiajs/react";
 import { useState } from "react";
 
-export default function Edit({ setOpenEditModal, hospital, setShowToast, setSuccessMessage }) {
+export default function Edit({
+    setOpenEditModal,
+    hospital,
+    setShowToast,
+    setSuccessMessage,
+    areas,
+}) {
     const [error, setError] = useState("");
-    const [hospitalName, setHospitalName] = useState(hospital.hospital_name || "")
+    const [hospitalName, setHospitalName] = useState(
+        hospital.hospital_name || ""
+    );
 
     return (
         <dialog open className="modal">
@@ -18,12 +26,17 @@ export default function Edit({ setOpenEditModal, hospital, setShowToast, setSucc
                         setOpenEditModal(false);
                         setShowToast(true);
                         setTimeout(() => setShowToast(false), 3000);
-                        setSuccessMessage(`${hospitalName} updated successfully`)
+                        setSuccessMessage(
+                            `${hospitalName} updated successfully`
+                        );
                     }}
                 >
-                    <div className="flex flex-col gap-2 mt-3">
+                    <div className="flex flex-col gap-2 mt-8">
                         <div className="flex justify-between">
-                            <label htmlFor="hospital_number" className="text-sm">
+                            <label
+                                htmlFor="hospital_number"
+                                className="text-sm"
+                            >
                                 Hospital No.
                             </label>
                             {error.hospital_number && (
@@ -41,7 +54,7 @@ export default function Edit({ setOpenEditModal, hospital, setShowToast, setSucc
                         />
                     </div>
 
-                    <div className="flex flex-col gap-2 mt-3">
+                    <div className="flex flex-col gap-2 mt-8">
                         <div className="flex justify-between">
                             <label htmlFor="hospital_name" className="text-sm">
                                 Hospital Name
@@ -62,10 +75,46 @@ export default function Edit({ setOpenEditModal, hospital, setShowToast, setSucc
                         />
                     </div>
 
-                    <div className="flex justify-end mt-6">
+                    <div className="flex flex-col gap-2 mt-8">
+                        <div className="flex justify-between">
+                            <label htmlFor="area_id" className="text-sm">
+                                Area
+                            </label>
+                            {error.area_id && (
+                                <span className="text-red-500 text-sm">
+                                    {error.area_id}
+                                </span>
+                            )}
+                        </div>
+                        <select
+                            defaultValue={hospital.area.area_name}
+                            className="select w-full"
+                            name="area_id"
+                            id="area_id"
+                        >
+                            <option value="" disabled>
+                                Select
+                            </option>
+                            {areas.map((area) => (
+                                <option key={area.id} value={area.id}>
+                                    {area.area_name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <div className="flex justify-end mt-6 gap-2">
+                        <button
+                            className="btn btn-outline rounded-xl"
+                            onClick={() => {
+                                setOpenEditModal(false);
+                            }}
+                        >
+                            Cancel
+                        </button>
                         <button
                             type="submit"
-                            className="btn bg-gray-800 text-white"
+                            className="btn bg-gray-800 text-white rounded-xl"
                         >
                             Confirm
                         </button>
