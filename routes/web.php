6d150b90f6d5152d966ff1f8bd9dbe7262c1a108
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2026-01-20 16:34:12 +0800

    Add permission middleware to route groups
    Introduced specific permission middleware to the hospitals, invoices, invoice history, user management, and import data route groups to enforce access control based on user permissions.

--- routes/web.php
+++ routes/web.php
@@ -18,37 +18,47 @@ Route::middleware(["guest"])->group(function () {
 });
 
 Route::middleware(["auth"])->group(function () {
-    Route::prefix("hospitals")->group(function () {
-            Route::get("/", [HospitalController::class, "index"]);
-            Route::post("/create", [HospitalController::class, "store"]);
-            Route::put("/edit/{id}", [HospitalController::class, "update"]);
-            Route::delete("/delete/{id}", [HospitalController::class, "destroy"]);
+    Route::prefix("hospitals")
+        ->middleware(["permission:view_all_hospitals,view_area_hospitals"])
+        ->group(function () {
+                Route::get("/", [HospitalController::class, "index"]);
+                Route::post("/create", [HospitalController::class, "store"]);
+                Route::put("/edit/{id}", [HospitalController::class, "update"]);
+                Route::delete("/delete/{id}", [HospitalController::class, "destroy"]);
 
-            Route::prefix("{hospital_id}/invoices")->group(function () {
-                Route::get("/", [InvoiceController::class, "index"]);
-                Route::post("/store", [InvoiceController::class, "store"]);
-                Route::put("/{invoice_id}/update", [InvoiceController::class, "update"]);
-                Route::post("/delete", [InvoiceController::class, "destroy"]);
+                Route::prefix("{hospital_id}/invoices")
+                    ->middleware(["permission:view_invoices"])
+                    ->group(function () {
+                        Route::get("/", [InvoiceController::class, "index"]);
+                        Route::post("/store", [InvoiceController::class, "store"]);
+                        Route::put("/{invoice_id}/update", [InvoiceController::class, "update"]);
+                        Route::post("/delete", [InvoiceController::class, "destroy"]);
 
-                Route::prefix("{invoice_id}/history")->group(function () {
-                    Route::get("/", [InvoiceHistoryController::class, "index"]);
-                    Route::get("/download", [InvoiceHistoryController::class, "download"]);
-                    Route::post("/store", [InvoiceHistoryController::class, "store"]);
+                        Route::prefix("{invoice_id}/history")
+                            ->middleware(["permission:view_invoice_history"])
+                            ->group(function () {
+                                Route::get("/", [InvoiceHistoryController::class, "index"]);
+                                Route::get("/download", [InvoiceHistoryController::class, "download"]);
+                                Route::post("/store", [InvoiceHistoryController::class, "store"]);
+                        });
                 });
-            });
     }); 
 
-    Route::prefix("user-management")->group(function () {
-            Route::get("/", [UserController::class, "index"]);
-            Route::post("/store", [UserController::class, "store"]);
-            Route::put("/{user_id}/update", [UserController::class, "update"]);
-            Route::delete("/{user_id}/delete", [UserController::class, "destroy"]);
+    Route::prefix("user-management")
+        ->middleware(["permission:view_users"])
+        ->group(function () {
+                Route::get("/", [UserController::class, "index"]);
+                Route::post("/store", [UserController::class, "store"]);
+                Route::put("/{user_id}/update", [UserController::class, "update"]);
+                Route::delete("/{user_id}/delete", [UserController::class, "destroy"]);
     });
 
-    Route::prefix("import-data")->group(function () {
-            Route::get("/", [ImportDataController::class, "index"]);
-            Route::get("/download-template", [ImportDataController::class, "downloadTemplate"]);
-            Route::post("/store", [ImportDataController::class, "store"]);
+    Route::prefix("import-data")
+        ->middleware(["permission:view_import_data"])
+        ->group(function () {
+                Route::get("/", [ImportDataController::class, "index"]);
+                Route::get("/download-template", [ImportDataController::class, "downloadTemplate"]);
+                Route::post("/store", [ImportDataController::class, "store"]);
     });
 
     Route::post("/logout", [AuthController::class, "destroy"]);
