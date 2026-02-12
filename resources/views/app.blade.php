a875f4a736f4cd0df6ae6673fed87850716b0b1c
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2026-02-12 10:22:14 +0800

    Change app title to Track Wise
    Update resources/views/app.blade.php to change the HTML title from 'Invoice Tracker' to 'Track Wise' to reflect the new app name/branding.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1">
 
-      <title>Invoice Tracker</title>
+      <title>Track Wise</title>
 
       @viteReactRefresh
       @vite('resources/js/app.jsx')
